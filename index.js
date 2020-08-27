4f2c5d854cee87bc2cc1a3d1d5c5a40a6bc4e261
Nikolas Lenning <nikolaslenning@gmail.com>
2020-08-26 19:52:29 -0600

    created function to create queryURL, added ajax call

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-var userCity = []
-var userCityString = userCity.toString();
-var APIKey= "5f768f5ae7bdd6d4ec9a50a55e103f63"
-queryURL= "api.openweathermap.org/data/2.5/weather?q="+ userCity +"&appid="+ APIKey;
+var APIKey = "5f768f5ae7bdd6d4ec9a50a55e103f63"
+// var userCity = $('#city-input');
 
+//U
+function URLFunction(cityName) {    
+    return `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`
+}
 
-
-
-
-
-
-console.log(userCity);    
-console.log(userCityString);    
-
-
-
-
-$('#submit-btn').on('click', function(event){
+$('#submit-btn').on('click', function (event) {
     event.preventDefault();
     var city = $('#city-input').val().trim();
-    userCity.push(city);
-    console.log(userCity);    
-    console.log(userCityString);    
+    var queryURL= URLFunction(city);
+    console.log(queryURL);
+     $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+    console.log(response);
+       
+
+
+
+
+
+
+
+
+
+
+    });
+    
+    
 });
