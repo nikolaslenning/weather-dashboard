24fc1ed0375ababaab00f0658b514241e1baa4e9
Nikolas Lenning <nikolaslenning@gmail.com>
2020-08-29 17:12:49 -0600

    added so much

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ function URLFunction(cityName) {
 
 $('#submit-btn').on('click', function (event) {
     event.preventDefault();
+
     var city = $('#city-input').val().trim();
     var queryURL = URLFunction(city);
-
+    console.log(event);
 
     // console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
+        // console.log(response);
         var iconCode = response.weather[0].icon
         var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
         var kelvin = response.main.temp;
@@ -25,51 +26,131 @@ $('#submit-btn').on('click', function (event) {
         $('#temp').text('Temperature: ' + fahrenheit.toFixed(1) + ' \xB0F');
         $('#humidity').text('Humidity: ' + response.main.humidity + '%');
         $('#wind').text('Wind Speed: ' + response.wind.speed + " MPH");
-        $('#image').attr('src', iconURL);
+        var iconImage = $('#image').attr('src', iconURL);
 
-        console.log(response.weather[0].icon);
         var latitude = response.coord.lat;
         var longitude = response.coord.lon;
         var APIKey = "5f768f5ae7bdd6d4ec9a50a55e103f63"
         var indexURL = indexFunction(latitude, longitude);
-        console.log(indexURL);
 
+        //Ajax call that takes UV index response value and sets background of UV index
         $.ajax({
             url: indexURL,
             method: "GET"
         }).then(function (response2) {
-            console.log(response2)
             $('#UVIndex').text('UV Index: ' + response2.current.uvi)
             if (response2.current.uvi < 3) {
                 $('#UVIndex').addClass('uvIndexGreen');
             }
             else if (response2.current.uvi >= 3 && response2.current.uvi < 6) {
-                $('#UVIndex').addClass('uvIndexYellow')
+                $('#UVIndex').addClass('uvIndexYellow');
             }
             else if (response2.current.uvi >= 6 && response2.current.uvi < 8) {
-                $('#UVIndex').addClass('uvIndexOrange')
+                $('#UVIndex').addClass('uvIndexOrange');
             }
             else if (response2.current.uvi >= 8) {
-                $('#UVIndex').addClass('uvIndexRed')
+                $('#UVIndex').addClass('uvIndexRed');
             }
+            console.log(response2);
+            
+            var dailyArray = response2.daily.slice(1, 6);
+            // console.log(dailyArray);
+            var fiveDayForecast = document.getElementById('five-day-forecast');
+            dailyArray.forEach(function (currentElement, index, array) {
+               // var newDiv= ''; // need to figure out how to clear div after each click
+                var newDiv = document.createElement('div');
+                var newH6 = document.createElement('h6');
+                var newIMG = document.createElement('img');
+                var newPTemp = document.createElement('p')
+                var newPHumidity = document.createElement('p')
+                var iconCode = currentElement.weather[0].icon;
+                var iconForecastURL = "http://openweathermap.org/img/w/" +iconCode+ ".png"
+
+                var forecastTimestamp = (currentElement.dt) *1000;
+                var forecastDateObject= new Date(forecastTimestamp);
+                var monthForecast = forecastDateObject.toLocaleString('en-US', {month: 'numeric'});
+                var dayForecast = forecastDateObject.toLocaleString("en-US", { day: "numeric" });
+                var yearForecast = forecastDateObject.toLocaleString("en-US", { year: "numeric" });
+                console.log(currentElement);
+                // newH6.innerHTML = convertTime(currentElement.dt)                
+                 
+                newH6.innerHTML =  monthForecast + '/' + dayForecast + "/" + yearForecast 
+                newDiv.setAttribute('class', 'newDiv card text-white bg-primary mb-3 col-2');
+                newIMG.setAttribute('class', 'icon');
+                // console.log('CURRENT ELEMENT at ' + index + '-->', currentElement)
+                // newH6.innerHTML ="Date: "+ currentElement.dt;
+                newIMG.setAttribute('src', iconForecastURL);
+                newPTemp.innerHTML = 'Temp: ' + ((currentElement.temp.max -273.15) * 1.8 +32).toFixed(1)+ ' \xB0F';
+                newPHumidity.innerHTML = 'Humidity: ' + currentElement.humidity+'%';
+               
+                newDiv.appendChild(newH6);
+                newDiv.appendChild(newIMG);
+                newDiv.appendChild(newPTemp);
+                newDiv.appendChild(newPHumidity);
+                // console.log(currentElement.weather[0].icon);
+                // console.log(iconForecastURL);
+
+                fiveDayForecast.appendChild(newDiv)
+            })
+            // var dailyArray = response2.daily.slice(1,6);
+            // console.log(dailyArray);
+            // var dailyWeatherList = document.getElementById('daily-weather-list')
+            // dailyArray.forEach(function(currentElement,index,array){
+            //     var newListItem = document.createElement('li')
+            //     newListItem.setAttribute('class', 'list-group-item')
+            //     console.log('CURRENT ELEMENT at ' + index + '-->', currentElement)
+            //     newListItem.innerHTML = currentElement.weather[0].description
+            //     dailyWeatherList.appendChild(newListItem)
+            // })
         });
 
-
+        // function that takes lat & long from first ajax request and makes 2nd ajax call for UV index
         function indexFunction() {
             return `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&
             exclude=minutely&appid=${APIKey}`
         }
+        // Function that converts Unix Timestamp to readable date
         function convertTime(timestamp) {
             var unixTimestamp = (response.dt) * 1000;
             var dateObject = new Date(unixTimestamp);
-            console.log(dateObject);
-            var dateFormat = dateObject.toLocaleString()
-            console.log(dateFormat);
-            dateObject.toLocaleString("en-US", { month: "numeric" }) 
-            dateObject.toLocaleString("en-US", { day: "numeric" })
-            dateObject.toLocaleString("en-US", { year: "numeric" })
+            var monthDate = dateObject.toLocaleString("en-US", { month: "numeric" });
+            var dayDate = dateObject.toLocaleString("en-US", { day: "numeric" });
+            var yearDate = dateObject.toLocaleString("en-US", { year: "numeric" });
+            $('#city').append(" (" + monthDate + '/' + dayDate + "/" + yearDate + ') ')
             //convert timestamp to date --- https://coderrocketfuel.com/article/convert-a-unix-timestamp-to-a-date-in-vanilla-javascript
         }
         convertTime();
+        $('#city').append(iconImage);
     });
+    // trying to set prepend local storage to page 
+    var localStorage = window.localStorage;
+    var dailyWeatherList = document.querySelector('#daily-weather-list')
+    var inputCity = $('#city-input').val()
+    // dailyWeatherList.forEach(function(citySelected){
+        var newLI = document.createElement('li');
+        localStorage.setItem('inputCity', JSON.stringify(inputCity));
+        // newLI.innerHTML = ;
+        console.log(inputCity);
+        console.log(localStorage);
+        newLI.innerHTML = inputCity;
+        dailyWeatherList.prepend(newLI);
+
+    // });
+    
+    
+
+
+    // var lastCity = JSON.parse('inputCity');
+    // var 
+    // localStorage.getItem(inputCity);
+    // $('#daily-weather-list').prepend(inputCity);
+    // $('.list-group-item').prepend(inputCity);
+     
+
+
+
+
+
+
+
 });
